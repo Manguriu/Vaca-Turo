3f7ffe9c7b8e72598011ad8cde6901b70d1a4466
@ManguriuManguriu <brianmiruri983@gmail.com>
2024-04-26 08:55:56 +0300

    change2

--- app/ExploreCarsMake/page.tsx
+++ app/ExploreCarsMake/page.tsx
@@ -3,10 +3,10 @@ import Explorecard from "./Explorecard";
 export default function ExploreCarsMake() {
   return (
     <main className="max-w-screen-xl mx-auto h-screen">
-      <div className="">
+      <div>
         <div className="flex flex-col justify-center items-center mt-20">
           <h2 className="font-bold">
-            Discover Turo, the worlds largest car sharing marketplace
+            Discover Turo the worlds largest car sharing marketplace
           </h2>
           <h1 className="text-4xl mt-4 font-bold">Rent a Jeep</h1>
         </div>
