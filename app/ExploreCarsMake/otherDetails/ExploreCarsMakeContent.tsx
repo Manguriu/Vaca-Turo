ffca00719d9cfc7e50992b6e1436b8f2f4d0eafd
@ManguriuManguriu <brianmiruri983@gmail.com>
2024-05-22 15:40:46 +0300

    err

--- app/ExploreCarsMake/otherDetails/ExploreCarsMakeContent.tsx
+++ app/ExploreCarsMake/otherDetails/ExploreCarsMakeContent.tsx
@@ -76,7 +76,7 @@ const ExploreCarsMakeContent = ({ selectedMake }: ExploreCarsMakeContentProps) =
       ) : (
         <div className="text-center">
           <h2 className="text-2xl font-bold">Car not found</h2>
-          <p>We couldn't find any cars for the selected make.</p>
+          <p>We couldnt find any cars for the selected make.</p>
           
         </div>
       )}
