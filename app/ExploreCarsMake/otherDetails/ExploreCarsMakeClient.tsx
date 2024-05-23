b61bc5329d03ca6723f5a3462ffc7e117eb29cfa
@ManguriuManguriu <brianmiruri983@gmail.com>
2024-05-23 09:39:21 +0300

    rendering error

--- app/ExploreCarsMake/otherDetails/ExploreCarsMakeClient.tsx
+++ app/ExploreCarsMake/otherDetails/ExploreCarsMakeClient.tsx
@@ -88,49 +88,59 @@ const ExploreCarsMakeClient = () => {
   }
 
   return (
-    <div className="flex justify-center gap-3 flex-wrap pt-4">
-      {filteredModels.length > 0 ? (
-        filteredModels.map((details) => (
-          <Link href={`/CarDetails/${details.id}`} key={details.id}>
-            <div className="card card-compact w-72 bg-base-100 shadow-xl mb-4 py-5">
-              <figure>
-                <img
-                  src={details.photo}
-                  alt="category"
-                  className="object-cover w-full h-36 rounded-t-lg"
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="text-lg font-bold">{details.title}</h2>
-                <h1 className="flex text-lg items-center gap-1">
-                  <span className="font-bold">{details.details.rating}</span>
-                  <span>
-                    <img src="/star.png" alt="" className="h-5" />
-                  </span>
-                </h1>
-                <hr className="border-t-2 border-gray-200 my-2" />
-                <div className="flex justify-between items-center">
-                  <div className="card-actions justify-start">
-                    <button className="btn btn-link font-bold">Book Now</button>
-                  </div>
-                  <div className="card-actions justify-end">
-                    <h1 className="text-lg">
-                      <span className="font-extrabold">{details.price}</span>
-                      <span> / day</span>
-                    </h1>
+    <div>
+      {" "}
+      <h1 className="flex justify-center text-4xl mt-4 font-bold">
+        Rent a {selectedMake}
+      </h1>
+      <div className="flex justify-center gap-3 flex-wrap pt-4">
+        {filteredModels.length > 0 ? (
+          filteredModels.map((details) => (
+            <Link href={`/CarDetails/${details.id}`} key={details.id}>
+              <div className="card card-compact w-72 bg-base-100 shadow-xl mb-4 py-5">
+                <figure>
+                  <img
+                    src={details.photo}
+                    alt="category"
+                    className="object-cover w-full h-36 rounded-t-lg"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="text-lg font-bold">{details.title}</h2>
+                  <h1 className="flex text-lg items-center gap-1">
+                    <span className="font-bold">{details.details.rating}</span>
+                    <span>
+                      <img src="/star.png" alt="" className="h-5" />
+                    </span>
+                  </h1>
+                  <hr className="border-t-2 border-gray-200 my-2" />
+                  <div className="flex justify-between items-center">
+                    <div className="card-actions justify-start">
+                      <button className="btn btn-link font-bold">
+                        Book Now
+                      </button>
+                    </div>
+                    <div className="card-actions justify-end">
+                      <h1 className="text-lg">
+                        <span className="font-extrabold">{details.price}</span>
+                        <span> / day</span>
+                      </h1>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        ))
-      ) : (
-        <div className="text-center h-[50vh]">
-          <img src="/notfound.jpg" className="mt-5" />
-          <h2 className=" mt-5 text-2xl font-bold">{selectedMake} not found</h2>
-          <p>We couldnt find any cars for the selected make.</p>
-        </div>
-      )}
+            </Link>
+          ))
+        ) : (
+          <div className="text-center h-[50vh]">
+            <img src="/notfound.jpg" className="mt-5" />
+            <h2 className=" mt-5 text-2xl font-bold">
+              {selectedMake} not found
+            </h2>
+            <p>We couldnt find any cars for the selected make.</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- app/ExploreCarsMake/page.tsx
+++ app/ExploreCarsMake/page.tsx
@@ -1,37 +1,7 @@
-// "use client";
-
-// import React, { Suspense } from "react";
-// import { useSearchParams } from "next/navigation";
-// import ExploreCarsMakeContent from "./otherDetails/ExploreCarsMakeClient";
-
-// export default function ExploreCarsMake() {
-//   const searchParams = useSearchParams();
-//   const selectedMake = searchParams.get("make");
-
-//   return (
-//     <main className="max-w-screen-xl mx-auto flex flex-col my-auto items-center">
-//       <div className="lg:mt-20 text-center max-md:mt-[4rem]">
-//         <h2 className="font-bold px-4 max-md:text-[20px]">
-//           Discover KingVac Rentals the worlds largest Car Renting marketplace
-//         </h2>
-//         <h1 className="text-4xl mt-4 font-bold">Rent a {selectedMake}</h1>
-//       </div>
-//       <div className="flex gap-6 justify-center items-center mt-8">
-//         <div className="flex justify-center items-center lg:px-4">
-//           <Suspense fallback={<div>Loading...</div>}>
-//             <ExploreCarsMakeContent selectedMake={selectedMake} />
-//           </Suspense>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
-
 "use client";
 
 import React, { Suspense } from "react";
 import dynamic from "next/dynamic";
-import { useSearchParams } from "next/navigation";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
@@ -44,8 +14,7 @@ const ExploreCarsMakeClient = dynamic(
 );
 
 export default function ExploreCarsMake() {
-  const searchParams = useSearchParams();
-  const selectedMake = searchParams.get("make");
+
   return (
     <main className="max-w-screen-xl mx-auto flex flex-col my-auto items-center">
       <div className="lg:mt-20 text-center max-md:mt-[4rem]">
@@ -59,15 +28,12 @@ export default function ExploreCarsMake() {
             fallback={
               <div>
                 <Skeleton />
-                <Skeleton count={5} /> Loading...
+                <Skeleton count={5} />{" "}
+                Loading...
               </div>
             }
           >
-            <div className="flex flex-col">
-            <h1 className=" flex items-center justify-center text-4xl mt-4 font-bold">Rent a {selectedMake}</h1>
             <ExploreCarsMakeClient />
-            </div>
-            
           </Suspense>
         </div>
       </div>
